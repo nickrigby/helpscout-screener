3ff77e17f2d43f990ba9b57b8b12ca2fb5d1d501
Nick Rigby <nick.rigby@gmail.com>
2020-12-31 09:54:26 -0500

    Add JSDoc function comment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Test if a string starts with an uppercase letter A-Z
+ * 
+ * @param {string} str 
+ */
 const strStartsWithUppercaseLetter = (str) => /^[A-Z]/.test(str);
 
 export { strStartsWithUppercaseLetter };
