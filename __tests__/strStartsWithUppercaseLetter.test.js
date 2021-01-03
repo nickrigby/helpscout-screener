9c28b0e5039b3bce0c9132f82bd3bd6002a37d42
Nick Rigby <nick.rigby@gmail.com>
2021-01-03 10:02:16 -0500

    More complete test coverage

--- __tests__/strStartsWithUppercaseLetter.test.js
+++ __tests__/strStartsWithUppercaseLetter.test.js
@@ -1,10 +1,26 @@
 import { strStartsWithUppercaseLetter } from "../index";
 
-test("string starting with uppercase letter returns true", () =>
-  expect(strStartsWithUppercaseLetter("Help Scout")).toBe(true));
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  .split("")
+  .map((letter) => [letter, true]);
 
-test("string starting with lowercase letter returns false", () =>
-  expect(strStartsWithUppercaseLetter("help scout")).toBe(false));
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  .split("")
+  .map((letter) => [letter, false]);
+
+test.each(uppercaseLetters)(
+  "string starting with %s returns true",
+  (a, expected) => {
+    expect(strStartsWithUppercaseLetter(a)).toBe(expected);
+  }
+);
+
+test.each(lowercaseLetters)(
+  "string starting with %s returns false",
+  (a, expected) => {
+    expect(strStartsWithUppercaseLetter(a)).toBe(expected);
+  }
+);
 
 test("empty string returns false", () =>
   expect(strStartsWithUppercaseLetter("")).toBe(false));
